c188e040fb63207faaf0c49db7d96847b54f5641
Dave <david.huwiler@hotmail.com>
2026-08-21 15:40:41 +0200

    impemented rotary encoder overflow (copilot)

--- Batmon-main/src/rotaryencoder.cpp
+++ Batmon-main/src/rotaryencoder.cpp
@@ -23,21 +23,51 @@ bool longpressDone = true;
 ESP32Encoder encoder;
 
 void setEncoderRange(uint32_t value, uint32_t min, uint32_t max) {
-    encoder.setCount(value);
     rotaryMinVal = min;
     rotaryMaxVal = max;
+
+    if (max >= min) {
+        uint64_t rangeSize = (uint64_t)max - (uint64_t)min + 1ULL;
+        rotaryModuloVal = rangeSize <= UINT32_MAX ? (uint32_t)rangeSize : 0xFFFFFFFF;
+    } else {
+        rotaryModuloVal = 0;
+    }
+
+    int32_t normalizedValue = value;
+    if (rotaryModuloVal != 0xFFFFFFFF && rotaryMaxVal >= rotaryMinVal) {
+        int64_t rangeSize = (int64_t)rotaryModuloVal;
+        int64_t offset = (int64_t)normalizedValue - (int64_t)rotaryMinVal;
+        int64_t wrapped = ((offset % rangeSize) + rangeSize) % rangeSize;
+        normalizedValue = (int32_t)(wrapped + (int64_t)rotaryMinVal);
+    } else if (normalizedValue < (int32_t)rotaryMinVal) {
+        normalizedValue = (int32_t)rotaryMinVal;
+    } else if (normalizedValue > (int32_t)rotaryMaxVal) {
+        normalizedValue = (int32_t)rotaryMaxVal;
+    }
+
+    encoder.setCount(normalizedValue);
 }
 
 uint32_t getEncoderValue() {
     int32_t val = encoder.getCount();
-    if (val < rotaryMinVal) {
-        val = rotaryMinVal;
+
+    if (rotaryModuloVal != 0xFFFFFFFF && rotaryMaxVal >= rotaryMinVal) {
+        int64_t rangeSize = (int64_t)rotaryModuloVal;
+        int64_t offset = (int64_t)val - (int64_t)rotaryMinVal;
+        int64_t wrapped = ((offset % rangeSize) + rangeSize) % rangeSize;
+        val = (int32_t)(wrapped + (int64_t)rotaryMinVal);
         encoder.setCount(val);
-    } else if (val > rotaryMaxVal) {
-        val = rotaryMaxVal;
+        return (uint32_t)val;
+    }
+
+    if (val < (int32_t)rotaryMinVal) {
+        val = (int32_t)rotaryMinVal;
+        encoder.setCount(val);
+    } else if (val > (int32_t)rotaryMaxVal) {
+        val = (int32_t)rotaryMaxVal;
         encoder.setCount(val);
     }
-    return val;
+    return (uint32_t)val;
 }
 enum enumScreenMode getScreenMode(enum enumScreenMode currentScreenMode) {
     if((buttonReleaseTime - buttonPressedTime) > 1000 && !longpressDone && encoderButtonPressed==false) {
